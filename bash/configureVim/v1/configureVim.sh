b1e38eccd9276c52d46e07e5783131d6403ef831
Jorge <jorge.salinas.jsf@outlook.com>
2023-07-30 14:18:29 -0500

    script to configure vim v1 - 2nd attempt, file already exists

--- bash/configureVim/v1/configureVim.sh
+++ bash/configureVim/v1/configureVim.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 file_location=~/.vimrc
-if [ -e $policy ]; then
-  echo "File $1.json already exists!"
-else
-  cat > $file_location <<EOF
+cat > $file_location <<EOF
 set nu
 set ic
 set ruler
